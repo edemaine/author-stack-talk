06b545e98f4e879618c6f20402762ca61ad058f6
Erik Demaine <edemaine@mit.edu>
2023-04-06 15:08:41 -0400

    Update 2023-04-06T19:08:39.968Z

--- stack.js
+++ stack.js
@@ -1,20 +1,23 @@
 (function() {
   document.querySelectorAll('.autostack').forEach((elt) => {
-    var i, match, name, names, year;
+    var i, match, name, names, split, year;
     //console.log elt
     [match, names, year] = elt.innerText.match(/^\s*([^\d]+)(?:\s+(\d+))?\s*$/);
-    return elt.innerHTML = `<div class="stack" title="${names}">
+    if (year == null) {
+      year = '';
+    }
+    split = names.split(/\s*;\s*/g);
+    return elt.innerHTML = `<div class="stack" title="${names} (${split.length}) ${year}">
 ${((function() {
-      var j, len, ref, results;
-      ref = names.split(/\s*;\s*/g);
+      var j, len, results;
       results = [];
-      for (i = j = 0, len = ref.length; j < len; i = ++j) {
-        name = ref[i];
+      for (i = j = 0, len = split.length; j < len; i = ++j) {
+        name = split[i];
         results.push((i ? '<div class="semi">;</div>' : '') + `<div class="name">${name}</div>`);
       }
       return results;
     })()).join('\n')}
-</div> ${year != null ? year : ''}`;
+</div> ${year}`;
   });
 
 }).call(this);
